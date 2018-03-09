0194e2a754d41fc7aa1061f2ed5d05ac7c3576c4
Roberto Garcia <robertogarcia@MacBook-Pro-de-Roberto.local>
2018-03-08 20:44:39 -0600

    Excercise Chapter3 World Trotter

---
+++ Exercises/WorldTrotter/WorldTrotter/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by Roberto Garcia on 3/7/18.
+//  Copyright © 2018 Roberto Garcia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //X Y are the values in a plane where it going to start drawing the frame
+        //width and height are the values of the frame
+        /*let firstFrame = CGRect(x: 160, y: 240, width: 100, height: 150)
+        let firstView = UIView(frame: firstFrame)
+        firstView.backgroundColor = UIColor.blue
+        view.addSubview(firstView)
+        
+        let secondFrame = CGRect(x: 20, y: 30, width: 50, height: 50)
+        let secondView = UIView(frame: secondFrame)
+        secondView.backgroundColor = UIColor.green
+        //view.addSubview(secondView)
+        //The second view became a subview of the first view
+        firstView.addSubview(secondView) */
+        
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
